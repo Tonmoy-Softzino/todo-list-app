bd508f09cada3e222e270c96a53e5cdd4e84506d
Tonmoy-softzino <pramaniktonmoy2@gmail.com>
2023-05-25 18:26:58 +0600

    modified

--- src/components/ListTemplate.vue
+++ src/components/ListTemplate.vue
@@ -6,14 +6,26 @@ const props = defineProps({
   task: Object,
 });
 
+// const editing = ref(false);
+// const editInput = ref(null);
 
-const deleteTask = () => {
-  props.task.deleted = true;
+const updateTask = () => {
+  editing.value = true;
+    // nextTick(() => {
+    //   editInput.value.focus();
+    // });
 };
 
-const updateTask = (task) => {
-  console.log('require to change the task');
-}
+// const textLength = computed(() => {
+//   return props.task.label?.length * 10 ?? 40;
+// });
+
+
+const deleteTask = () => {
+  const res = confirm("Are you sure to delete the task?");
+  if (res) props.task.deleted = true;
+};
+
 </script>
 
 <!-- Script End here and HTML start -->
@@ -27,15 +39,28 @@ const updateTask = (task) => {
           type="checkbox"
           v-model="task.completed"
         />
-        <span> {{ task.label }}</span>
+        <span> 
+          <!-- <input
+          :style="{ width: textLength + 'px' }"
+          ref="editInput"
+          type="text"
+          :placeholder="task.label"
+          :disabled="!editing"
+          v-model="task.label"
+          @keydown.enter="editing = false"
+          @focusout="editing = false"
+          /> -->
+          {{ task.label }}
+        </span>
       </div>
       <div class="button">
         <button v-show="!task.deleted" type="button" @click="deleteTask">
           Delete
         </button>
-        <button v-show="!task.deleted" type="button" @click="updateTask(task.label)">
+        <!-- <button 
+         type="button" @click="updateTask()">
           Update
-        </button>
+        </button> -->
       </div>
     </label>
   </li>
